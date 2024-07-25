b16cc15f9d2e5257750b875b91d167b8945ab24a
Andrew Tran <Andrew.Tran2@cerner.com>
2024-07-25 14:56:24 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import './App.css';
 import DiffPage from './components/DiffPage';
 
 function App() {
-  return (
-    <>
-      <DiffPage />
-    </>
-  );
+  return <DiffPage />;
 }
 
 export default App;
